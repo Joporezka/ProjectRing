71443bbe6bb19cbd4ffb5b9a29e705eb58659ab1
Joporezka <32361833+Joporezka@users.noreply.github.com>
2020-12-03 21:59:10 +0600

    v0.4 settings work

--- ProjectRing.ino
+++ ProjectRing.ino
@@ -18,8 +18,8 @@ MicroDS3231 rtc;
 const uint8_t ptr_bmp[] PROGMEM = {
   0x3C, 0x3C, 0x3C, 0x3C, 0x3C, 0xFF, 0xFF, 0x7E, 0x3C, 0x18,
 };
-uint8_t TimeSettings[]= {       // массив значений настроек
-        8, 00, 40, 10, 10, 20, 10, 10           //часы начала, минуты начала, продолжительность урока, n(5) перемен
+uint8_t TimeSettings[] = {      // массив значений настроек
+  8, 00, 40, 10, 10, 20, 10, 10           //часы начала, минуты начала, продолжительность урока, n(5) перемен
 };
 bool MainSignal = false;
 
@@ -34,11 +34,11 @@ void loop() {
 
 
   enc.tick();
-  if (enc.isLeft() or enc.isFastL()) {
+  if (enc.isLeft()) {
     pointer = constrain(pointer - 1, 0, ITEMS - 1);
   }
-
-  if (enc.isRight() or enc.isFastR()) {
+  enc.tick();
+  if (enc.isRight()) {
     pointer = constrain(pointer + 1, 0, ITEMS - 1);
   }
 
@@ -76,6 +76,11 @@ void printPointer(uint8_t pointer) {
   oled.setCursor(0, pointer);
   oled.print(">");
 
+}
+
+void printActivePointer(uint8_t pointer) {
+  oled.setCursor(0, pointer);
+  oled.print(">!");
 }
 
 // ==========================================ВЫВОД ВРЕМЕНИ В СЕРИАЛ====================================
@@ -122,44 +127,71 @@ void CurrentTime(void) {
     oled.print(rtc.getMinutes());
     oled.print(":");
     oled.println(rtc.getSeconds());
-    
+
     oled.scale1X();
     PrintWeekDay();
     //oled.println(rtc.getDay());
-    if (MainSignal == 0){
-        oled.print("Ожидание...\n");
-    }else{
-        oled.print("Звоню!\n");
+    if (MainSignal == 0) {
+      oled.print("Ожидание...\n");
+    } else {
+      oled.print("Звоню!\n");
     }
 
     oled.update();
   }
 }
 
-void PrintWeekDay(){
-    switch(rtc.getDay()){
-        case 1: oled.println("Понедельник"); break;
-        case 2: oled.println("Вторник"); break;
-        case 3: oled.println("Среда"); break;
-        case 4: oled.println("Четверг"); break;
-        case 5: oled.println("Пятница"); break;
-        case 6: oled.println("Суббота"); break;
-        case 7: oled.println("Воскресенье"); break;
+void PrintWeekDay() {
+  switch (rtc.getDay()) {
+    case 1: oled.println("Понедельник"); break;
+    case 2: oled.println("Вторник"); break;
+    case 3: oled.println("Среда"); break;
+    case 4: oled.println("Четверг"); break;
+    case 5: oled.println("Пятница"); break;
+    case 6: oled.println("Суббота"); break;
+    case 7: oled.println("Воскресенье"); break;
 
-    }
+  }
 
 }
 
-void settings(void){
-    oled.clear();
-    oled.home();
-    oled.print
-    (F(
-            "   parameter 1: \n"
-            "   parameter 2: \n"
-            ));
-    oled.update();
-    while(1){
-        if (enc.isClick()) return;
-    }
+//==============================МЕНЮ НАСТРОЕК===========================
+void settings(void) {
+  while(1){
+    
+  }
+  static uint8_t pointerSettings = 0;
+  oled.clear();
+  oled.home();
+  oled.print
+  (F(
+     "   Час начала: \n"
+     "   Мин. начала: \n"
+     "   Длительность: \n"
+     "   Перемена 1: \n"
+     "   Перемена 2: \n"
+     "   Перемена 3: \n"
+     "   Перемена 4: \n"
+     "   Перемена 5: \n"
+   ));
+  for (uint8_t i = 0; i < 8; i++) {
+    oled.setCursor(16, i);
+    oled.print(TimeSettings[i]);
+  }
+
+  enc.tick();
+  if (enc.isLeft()) {
+    pointerSettings = constrain(pointerSettings - 1, 0, 7);
+  }
+  if (enc.isRight()) {
+    pointerSettings = constrain(pointerSettings + 1, 0, 7);
+  }
+  printPointer(pointerSettings);
+  
+
+  oled.update();
+  while (1) {
+    enc.tick();
+    if (enc.isClick()) return;
+  }
 }
